c6ed6639c364c48e1fc29b426e538b4b89f67310
ido rozin <rozin.ido@gmail.com>
2018-10-14 15:20:30 +0300

    Heart UI + Play Services

---
+++ GooglePlay/PlayServices.cs
@@ -0,0 +1,60 @@
+﻿/*
+
+using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class PlayServices : MonoBehaviour {
+
+    // ... other code here... 
+    public void Start() {
+        // Create client configuration
+        PlayGamesClientConfiguration config = new 
+            PlayGamesClientConfiguration.Builder()
+            .Build();
+
+        // Enable debugging output (recommended)
+        PlayGamesPlatform.DebugLogEnabled = true;
+        
+        // Initialize and activate the platform
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        
+    }
+    public void SignInCallback(bool success) {}
+
+
+    #region Leaderboard
+    public void ShowLeaderboards() {
+        if (PlayGamesPlatform.Instance.localUser.authenticated) {
+            PlayGamesPlatform.Instance.ShowLeaderboardUI();
+        }
+        else {
+            Debug.Log("Cannot show leaderboard: not authenticated");
+        }
+    }
+
+    public void addScoreToLeaderboard(string leaderboardID,int score)
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            // Note: make sure to add 'using GooglePlayGames'
+            PlayGamesPlatform.Instance.ReportScore(score,
+                GPGSIds.leaderboardID,
+                (bool success) =>
+                {
+                    Debug.Log("(game) Leaderboard update success: " + success);
+                });
+        }
+
+    }
+    #endregion
+   
+ }
+*/
+
+
+
+
---
+++ HeartsUi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsUi : MonoBehaviour
+{
+	public static HeartsUi heartsUi;
+
+	private void Awake()
+	{
+		heartsUi = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		setInitialHearts(3);
+	}
+
+	[SerializeField]
+	private GameObject heart;
+	[SerializeField]
+	private GameObject PanelHearts;
+	[SerializeField] 
+	private Color fadedHeartColor;
+	
+	
+	public void setInitialHearts(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+			Instantiate(heart,PanelHearts.transform);
+	}
+
+	public void heartsDown(int index)
+	{
+		PanelHearts.transform.GetChild(index).GetComponent<Image>().color = fadedHeartColor;
+	}
+}

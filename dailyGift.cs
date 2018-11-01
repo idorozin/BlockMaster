9eb1c98e4064bb201aed286ea90af9bc66308c31
ido rozin <rozin.ido@gmail.com>
2018-11-01 17:13:06 +0200

    swipe detector , trailEffect , and a lot of unfinished stuff.

---
+++ dailyGift.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dailyGift : MonoBehaviour {
+	
+	[SerializeField]
+	Transform[] ticks = new Transform[30];
+	[SerializeField]
+	string[] gifts = new string[30];
+	private bool timePassed=true;
+	private int index=0;
+
+	public void getGiftButton()
+	{
+		if (!timePassed)
+			return;
+		index++;
+		getGift(gifts[index]);
+		
+	}
+
+	private void getGift(string gift)
+	{
+		if (gift == "200coins")
+			PlayerStats.money += 200;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ dowloadUpdates.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dowloadUpdates : MonoBehaviour
+{
+	private string path="http://35.242.248.201/test.php";
+     IEnumerator Start ()
+     {
+	     WWW www = new WWW(path);
+	     yield return www;
+	     Debug.Log("[ITS UPDATED]NEW UPDATES:" + www.text.ToString());
+     }
+
+}

c52a115fd889e230636c298c5bf090543bc669b9
ido rozin <rozin.ido@gmail.com>
2018-12-04 12:09:05 +0200

    DailyReward2 + Start Of Challanges

---
+++ Challange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Challange : MonoBehaviour
+{
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	public static Challange Instance;
+
+	[SerializeField]private GameObject Challanges;
+
+	// Use this for initialization
+	public void LoadChallanges ()
+	{
+		PlayerStats.Instance.playerStats.SetChallanges();
+		int i = 0;
+		foreach (Transform child in Challanges.transform)
+		{
+			foreach (Transform text in child.transform)
+			{
+				if (text.gameObject.name == "ChallangeTxt")
+					text.gameObject.GetComponent<TextMeshProUGUI>().text=PlayerStats.Instance.playerStats.cs[i].challageText;
+				if(text.gameObject.name=="PrizeTxt")
+					text.gameObject.GetComponent<TextMeshProUGUI>().text=PlayerStats.Instance.playerStats.cs[i].reward();
+			}
+
+			i++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DailyReward2.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class DailyReward2: MonoBehaviour
+{
+	[SerializeField]
+	private int countDownLenght=500,coolDown;
+
+	[SerializeField] private GameObject TimeText, Button;
+	public static string timeText;
+	public static bool run = false , GiftAllowed=false;
+	private void Start()
+	{
+		Debug.Log(PlayerStats.Instance.playerStats.giftTime + "GIFT");
+		if (PlayerStats.Instance.playerStats.giftTime == "")
+		{
+			TimeText.GetComponent<Text>().text = "READY!";
+			GiftAllowed = true;
+			return;
+		}
+
+		coolDown = 2;
+		StartCoroutine("CountDown");
+	}
+	
+	//reset the timer every roll
+	public IEnumerator resetTimer()
+	{
+		Debug.Log("reset");
+		yield return TimeManager.Instance.StartCoroutine("getTime");
+		PlayerStats.Instance.playerStats.offsetG = TimeManager.Instance.getTimeInSecs(DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss")) - TimeManager.Instance.getTimeInSecs();
+		coolDown = countDownLenght;
+		PlayerStats.Instance.playerStats.giftTime = TimeManager.Instance.getFullTime();
+		PlayerStats.Instance.saveFile();
+		StartCoroutine("CountDown");
+
+		Debug.Log(PlayerStats.Instance.playerStats.giftTime);
+	}
+
+	public void updateTime() // updates countDown with internet time
+	{
+		coolDown = (countDownLenght) - (TimeManager.Instance.getTimeInSecs() - TimeManager.Instance.getTimeInSecs(PlayerStats.Instance.playerStats.giftTime));
+	}
+
+	IEnumerator CountDown()
+	{
+		run = true;
+		while (coolDown > 0 || coolDown > countDownLenght+1)
+		{
+			Debug.Log("COUNT");
+			coolDown = timeRemaining();
+			if(TimeText != null && coolDown > 0)
+			TimeText.GetComponent<Text>().text = secsToTime();
+			timeText = secsToTime();
+			yield return new WaitForSeconds(1);
+		}
+		StartCoroutine("enableButton");
+	}
+
+	// if countDown is over check if verify that with the server
+	// true => button enabled false => button disabled and count down continouse with updated time. 
+	IEnumerator enableButton()
+	{
+		Debug.Log("enable");
+
+		if(TimeText!=null){TimeText.GetComponent<Text>().text = "READY!";timeText = secsToTime();}
+		//validate 
+		yield return TimeManager.Instance.StartCoroutine("getTime");
+		if (coolDown <= 0 && TimeText != null)
+		{
+			GiftAllowed = true;
+			yield break;
+		}
+		PlayerStats.Instance.playerStats.offsetG = TimeManager.Instance.getTimeInSecs(DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss")) - TimeManager.Instance.getTimeInSecs();
+		PlayerStats.Instance.saveFile();
+		//start timer again
+		StartCoroutine("CountDown");
+
+	}
+
+	 public string secsToTime() // convert seconds to time format 00:00:00
+	{
+		return coolDown / 60 / 60 + ":" + coolDown / 60 % 60 + ":" + coolDown % 60;
+	}
+		
+
+	int timeRemaining()
+	{
+		return countDownLenght - (-TimeManager.Instance.getTimeInSecs(PlayerStats.Instance.playerStats.giftTime) + 
+		                  TimeManager.Instance.getTimeInSecs(DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss"))) + PlayerStats.Instance.playerStats.offsetG;
+	}
+
+
+}

95684079a4f1c958445ba3b525bf7d6efc3ebf2d
ido rozin <rozin.ido@gmail.com>
2019-05-26 22:29:45 +0300

    Shop custon editor load folder feature

--- Assets/Scripts/Shop/Editor/CustomCatalogEditor.cs
+++ Assets/Scripts/Shop/Editor/CustomCatalogEditor.cs
@@ -1,17 +1,34 @@
 ﻿
+using System;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine.UI;
 
 [CustomEditor(typeof(ItemsShopCatalog))]
 public class CustomCatalogEditor : Editor
 {
    public Item.ItemType types;
+   public bool stop = false;
+   private string path;
    public override void OnInspectorGUI()
    {
       ItemsShopCatalog itemsShop = (ItemsShopCatalog)target;
       EditorGUILayout.BeginHorizontal();
+      path = TextField("Path" , path);
+      if (GUILayout.Button("Load From Folder"))
+      {
+         Sprite[] textures = Resources.LoadAll<Sprite>(path);
+         itemsShop.serializedItems.Add(new ListWraper());
+         foreach (Sprite texture in textures)
+         {
+            itemsShop.serializedItems[itemsShop.serializedItems.Count-1].list.Add(new Item(texture.name , texture));
+         }
+      }
+
       if (GUILayout.Button("Reset"))
       {
          if(itemsShop.serializedItems.Count > 0 && EditorUtility.DisplayDialog("", "Are you sure?" ,"Reset" , "Cancel"))
@@ -27,7 +44,7 @@ public class CustomCatalogEditor : Editor
       foreach (var category in itemsShop.serializedItems.ToList())
       {
          EditorGUILayout.BeginHorizontal();
-         category.type = (Item.ItemType)EditorGUILayout.EnumPopup(category.type);
+         category.type = TextField("Name",category.type);
          if (GUILayout.Button("Add Item"))
          {
             category.list.Add(new Item());
@@ -45,7 +62,7 @@ public class CustomCatalogEditor : Editor
             item.Diamonds = EditorGUILayout.IntField("Gems",item.Diamonds);
             item.Score = EditorGUILayout.IntField("Score",item.Score);
             item.Icon = (Sprite)EditorGUILayout.ObjectField("Icon", item.Icon, typeof(Sprite), allowSceneObjects: true);
-            item.type = category.type;
+            //item.type = category.type;
             if (GUILayout.Button("X"))
             {
                category.list.Remove(item);
@@ -57,12 +74,12 @@ public class CustomCatalogEditor : Editor
          GUILayout.Space(50);
       }
    }
-   
+
    public static string TextField(string label, string text)
    {
       var textDimensions = GUI.skin.label.CalcSize(new GUIContent(label));
       EditorGUIUtility.labelWidth = textDimensions.x;
       return EditorGUILayout.TextField(label, text);
    }
-   
+
 }
